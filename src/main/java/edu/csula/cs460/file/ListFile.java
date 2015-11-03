5e4d86310dd47d5442afb3dd721a986849fa0373
Niku-Ya <deathrenewed@gmail.com>
2015-11-02 17:21:15 -0800

    Revert "Name Change"
    This reverts commit 387988612e3dd61ce3f51e76962296d53738bed9.

--- src/main/java/edu/csula/cs460/file/ListFile.java
+++ src/main/java/edu/csula/cs460/file/ListFile.java
@@ -1,9 +1,5 @@
 package edu.csula.cs460.file;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
@@ -13,36 +9,13 @@ import com.google.common.collect.Lists;
 public class ListFile {
     private Map<String, List<String>> adjancencyList;
 
-    @SuppressWarnings("null")
-	public ListFile(String filepath) {
+    public ListFile(String filepath) {
         // TODO: read file from filepath ('exercise-1/list-1.txt' for
         // example) and parse line by line to fill out adjancencyList
-    	try {
-			BufferedReader br = new BufferedReader( new FileReader("src/main/resources/exercise-1/list-1.txt"/*+filepath*/));
-			
-			String line = br.readLine();
-			while(line != null){
-				List<String> list = new ArrayList<String>();
-				String[] lineChanger = line.replaceAll("\\:", " ").split(" ");
-				for(int i = 1; i < lineChanger.length; i++){
-					System.out.println(lineChanger[i]);
-					System.out.println(lineChanger[0] + " <-- AYE: " + i);
-					list.add(lineChanger[i]);
-				}
-				System.out.println(list.toString() + " LISTO");
-				adjancencyList.put(lineChanger[0], list);
-				System.out.println(adjancencyList.containsKey("a") + " ZE MAP");
-				line = br.readLine();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
     }
 
     public List<String> getList(String key) {
         // TODO: get List of String for specific key
-        return adjancencyList.get(key);
+        return Lists.newArrayList();
     }
 }
